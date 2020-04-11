3ce071f8f3d93968758109756e95b46646fffbc7
yrizhkov <yrizhkov@aerospike.com>
2020-04-11 14:40:57 +0300

    align swagger documentation with all supported query parameters

--- src/main/java/com/aerospike/restclient/util/annotations/ASRestClientBatchPolicyQueryParams.java
+++ src/main/java/com/aerospike/restclient/util/annotations/ASRestClientBatchPolicyQueryParams.java
@@ -16,30 +16,115 @@
  */
 package com.aerospike.restclient.util.annotations;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import com.aerospike.restclient.util.AerospikeAPIConstants;
 import com.aerospike.restclient.util.QueryParamDescriptors;
-
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /* Annotation which tells swagger to render all of the Batch Policy options as query parameters */
-@ApiImplicitParams(value= {
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.SEND_KEY,
-				dataType="boolean",
-				paramType="query",
-				value=QueryParamDescriptors.POLICY_SEND_KEY_NOTES,
-				defaultValue=QueryParamDescriptors.POLICY_SEND_KEY_DEFAULT),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.REPLICA,
-				dataType="string",
-				paramType="query",
-				allowableValues=QueryParamDescriptors.POLICY_REPLICA_ALLOWABLE_VALUES,
-				value=QueryParamDescriptors.POLICY_REPLICA_NOTES,
-				defaultValue=QueryParamDescriptors.POLICY_REPLICA_DEFAULT)
+@ApiImplicitParams(value = {
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SEND_KEY,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SEND_KEY_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SEND_KEY_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.REPLICA,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_REPLICA_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_REPLICA_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_REPLICA_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.KEY_TYPE,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.KEYTYPE_ALLOWABLE_VALUES,
+                defaultValue = QueryParamDescriptors.KEYTYPE_DEFAULT,
+                value = QueryParamDescriptors.KEYTYPE_NOTES),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.RECORD_BINS,
+                paramType = "query",
+                value = QueryParamDescriptors.BINS_NOTES,
+                dataType = "string", required = false, allowMultiple = true),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.READ_MODE_SC,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_READMODESC_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_READMODESC_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_READMODESC_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.READ_MODE_AP,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_READMODEAP_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_READMODEAP_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_READMODEAP_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.TOTAL_TIMEOUT,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_TOTAL_TIMEOUT_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_TOTAL_TIMEOUT_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SOCKET_TIMEOUT,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SOCKET_TIMEOUT_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SOCKET_TIMEOUT_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SLEEP_BETWEEN_RETRIES,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SLEEP_BETWEEN_RETRIES_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SLEEP_BETWEEN_RETRIES_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.MAX_RETRIES,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_MAX_RETRIES_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_MAX_RETRIES_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.PRED_EXP,
+                dataType = "string",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_PRED_EXP_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_PRED_EXP_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.COMPRESS,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_COMPRESS_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_COMPRESS_DEFAULT),
+
+        // Batch Operation Policies
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.ALLOW_INLINE,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.BATCH_POLICY_ALLOW_INLINE_NOTES,
+                defaultValue = QueryParamDescriptors.BATCH_POLICY_ALLOW_INLINE_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.MAX_CONCURRENT_THREADS,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.BATCH_POLICY_MAX_CONCURRENT_THREADS_NOTES,
+                defaultValue = QueryParamDescriptors.BATCH_POLICY_MAX_CONCURRENT_THREADS_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SEND_SET_NAME,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.BATCH_POLICY_SEND_SET_NAME_NOTES,
+                defaultValue = QueryParamDescriptors.BATCH_POLICY_SEND_SET_NAME_DEFAULT),
 })
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ASRestClientBatchPolicyQueryParams {}
+public @interface ASRestClientBatchPolicyQueryParams {
+}
--- src/main/java/com/aerospike/restclient/util/annotations/ASRestClientPolicyQueryParams.java
+++ src/main/java/com/aerospike/restclient/util/annotations/ASRestClientPolicyQueryParams.java
@@ -16,45 +16,94 @@
  */
 package com.aerospike.restclient.util.annotations;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import com.aerospike.restclient.util.AerospikeAPIConstants;
 import com.aerospike.restclient.util.QueryParamDescriptors;
-
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 
-@ApiImplicitParams(value= {
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.SEND_KEY, dataType="boolean", paramType="query",
-				value=QueryParamDescriptors.POLICY_SEND_KEY_NOTES, defaultValue=QueryParamDescriptors.POLICY_SEND_KEY_DEFAULT),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.REPLICA, dataType="string",
-				paramType="query", allowableValues=QueryParamDescriptors.POLICY_REPLICA_ALLOWABLE_VALUES,
-				value=QueryParamDescriptors.POLICY_REPLICA_NOTES, defaultValue=QueryParamDescriptors.POLICY_REPLICA_DEFAULT),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.KEY_TYPE, dataType="string",
-				paramType="query",
-				allowableValues=QueryParamDescriptors.KEYTYPE_ALLOWABLE_VALUES,
-				defaultValue=QueryParamDescriptors.KEYTYPE_DEFAULT,
-				value=QueryParamDescriptors.KEYTYPE_NOTES),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.RECORD_BINS, paramType="query",
-				value=QueryParamDescriptors.BINS_NOTES,
-				dataType="string", required=false, allowMultiple=true),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.READ_MODE_SC, dataType="string",
-				paramType="query",
-				allowableValues=QueryParamDescriptors.POLICY_READMODESC_ALLOWABLE_VALUES,
-				value=QueryParamDescriptors.POLICY_READMODESC_NOTES,
-				defaultValue=QueryParamDescriptors.POLICY_READMODESC_DEFAULT),
-		@ApiImplicitParam(
-				name=AerospikeAPIConstants.READ_MODE_AP, dataType="string",
-				paramType="query",
-				allowableValues=QueryParamDescriptors.POLICY_READMODEAP_ALLOWABLE_VALUES,
-				value=QueryParamDescriptors.POLICY_READMODEAP_NOTES,
-				defaultValue=QueryParamDescriptors.POLICY_READMODEAP_DEFAULT),
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@ApiImplicitParams(value = {
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SEND_KEY,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SEND_KEY_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SEND_KEY_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.REPLICA,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_REPLICA_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_REPLICA_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_REPLICA_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.KEY_TYPE,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.KEYTYPE_ALLOWABLE_VALUES,
+                defaultValue = QueryParamDescriptors.KEYTYPE_DEFAULT,
+                value = QueryParamDescriptors.KEYTYPE_NOTES),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.RECORD_BINS,
+                paramType = "query",
+                value = QueryParamDescriptors.BINS_NOTES,
+                dataType = "string", required = false, allowMultiple = true),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.READ_MODE_SC,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_READMODESC_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_READMODESC_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_READMODESC_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.READ_MODE_AP,
+                dataType = "string",
+                paramType = "query",
+                allowableValues = QueryParamDescriptors.POLICY_READMODEAP_ALLOWABLE_VALUES,
+                value = QueryParamDescriptors.POLICY_READMODEAP_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_READMODEAP_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.TOTAL_TIMEOUT,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_TOTAL_TIMEOUT_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_TOTAL_TIMEOUT_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SOCKET_TIMEOUT,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SOCKET_TIMEOUT_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SOCKET_TIMEOUT_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.SLEEP_BETWEEN_RETRIES,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_SLEEP_BETWEEN_RETRIES_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_SLEEP_BETWEEN_RETRIES_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.MAX_RETRIES,
+                dataType = "int",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_MAX_RETRIES_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_MAX_RETRIES_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.PRED_EXP,
+                dataType = "string",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_PRED_EXP_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_PRED_EXP_DEFAULT),
+        @ApiImplicitParam(
+                name = AerospikeAPIConstants.COMPRESS,
+                dataType = "boolean",
+                paramType = "query",
+                value = QueryParamDescriptors.POLICY_COMPRESS_NOTES,
+                defaultValue = QueryParamDescriptors.POLICY_COMPRESS_DEFAULT),
 })
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ASRestClientPolicyQueryParams {}
+public @interface ASRestClientPolicyQueryParams {
+}
