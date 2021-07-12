767bc14c6660b67dc8996b1c8be752fe065442a1
yrizhkov <yrizhkov@aerospike.com>
2021-07-12 09:48:04 +0300

    initial Document API support

---
+++ src/main/java/com/aerospike/restclient/handlers/DocumentHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements WHICH ARE COMPATIBLE WITH THE APACHE LICENSE, VERSION 2.0.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.aerospike.restclient.handlers;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Key;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
+import com.aerospike.documentapi.AerospikeDocumentClient;
+import com.aerospike.restclient.util.RestClientErrors;
+
+public class DocumentHandler {
+
+    private final AerospikeDocumentClient client;
+
+    public DocumentHandler(AerospikeClient client) {
+        this.client = new AerospikeDocumentClient(client);
+    }
+
+    public Object getObject(Key key, String binName, String jsonPath, Policy policy) {
+        try {
+            return client.get(policy, key, binName, jsonPath);
+        } catch (Exception e) {
+            throw new RestClientErrors.InvalidOperationError(e.getMessage());
+        }
+    }
+
+    public void putObject(Key key, String binName, String jsonPath, Object jsonObject, WritePolicy policy) {
+        try {
+            client.put(policy, key, binName, jsonPath, jsonObject);
+        } catch (Exception e) {
+            throw new RestClientErrors.InvalidOperationError(e.getMessage());
+        }
+    }
+
+    public void appendObject(Key key, String binName, String jsonPath, Object jsonObject, WritePolicy policy) {
+        try {
+            client.append(policy, key, binName, jsonPath, jsonObject);
+        } catch (Exception e) {
+            throw new RestClientErrors.InvalidOperationError(e.getMessage());
+        }
+    }
+
+    public void deleteObject(Key key, String binName, String jsonPath, WritePolicy policy) {
+        try {
+            client.delete(policy, key, binName, jsonPath);
+        } catch (Exception e) {
+            throw new RestClientErrors.InvalidOperationError(e.getMessage());
+        }
+    }
+
+    public static DocumentHandler create(AerospikeClient client) {
+        return new DocumentHandler(client);
+    }
+}
---
+++ src/main/java/com/aerospike/restclient/service/AerospikeDocumentServiceV1.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements WHICH ARE COMPATIBLE WITH THE APACHE LICENSE, VERSION 2.0.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.aerospike.restclient.service;
+
+import com.aerospike.client.Key;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
+import com.aerospike.restclient.domain.auth.AuthDetails;
+import com.aerospike.restclient.handlers.DocumentHandler;
+import com.aerospike.restclient.util.AerospikeAPIConstants;
+import com.aerospike.restclient.util.AerospikeClientPool;
+import com.aerospike.restclient.util.KeyBuilder;
+import com.aerospike.restclient.util.RestClientErrors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class AerospikeDocumentServiceV1 implements AerospikeDocumentService {
+
+    @Autowired
+    private AerospikeClientPool clientPool;
+
+    @Override
+    public Object getObject(AuthDetails authDetails, String namespace, String set, String key, String binName,
+                            String jsonPath, AerospikeAPIConstants.RecordKeyType keyType, Policy policy) {
+        Key asKey = KeyBuilder.buildKey(namespace, set, key, keyType);
+        return DocumentHandler.create(clientPool.getClient(authDetails)).getObject(asKey,
+                binName, decodeJsonPath(jsonPath), policy);
+    }
+
+    @Override
+    public void putObject(AuthDetails authDetails, String namespace, String set, String key, String binName,
+                          String jsonPath, Object jsonObject, AerospikeAPIConstants.RecordKeyType keyType, WritePolicy policy) {
+        Key asKey = KeyBuilder.buildKey(namespace, set, key, keyType);
+        DocumentHandler.create(clientPool.getClient(authDetails)).putObject(asKey,
+                binName, decodeJsonPath(jsonPath), jsonObject, policy);
+    }
+
+    @Override
+    public void appendObject(AuthDetails authDetails, String namespace, String set, String key, String binName,
+                             String jsonPath, Object jsonObject, AerospikeAPIConstants.RecordKeyType keyType, WritePolicy policy) {
+        Key asKey = KeyBuilder.buildKey(namespace, set, key, keyType);
+        DocumentHandler.create(clientPool.getClient(authDetails)).appendObject(asKey,
+                binName, decodeJsonPath(jsonPath), jsonObject, policy);
+    }
+
+    @Override
+    public void deleteObject(AuthDetails authDetails, String namespace, String set, String key, String binName,
+                             String jsonPath, AerospikeAPIConstants.RecordKeyType keyType, WritePolicy policy) {
+        Key asKey = KeyBuilder.buildKey(namespace, set, key, keyType);
+        DocumentHandler.create(clientPool.getClient(authDetails)).deleteObject(asKey,
+                binName, decodeJsonPath(jsonPath), policy);
+    }
+
+    private String decodeJsonPath(String jsonPath) {
+        try {
+            return URLDecoder.decode(jsonPath, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new RestClientErrors.InvalidOperationError("Invalid jsonPath parameter.");
+        }
+    }
+}
