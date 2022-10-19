9771c973aeb648c6cb5d4b5b8972cbbc6ccdf36e
Jesse S <jschmidt@aerospike.com>
2022-10-19 11:56:15 -0700

    Upgrade springboot from 2.5.12 to 2.7.4 (#84)
    * chore: Upgrade springboot to 2.7.4
    This requires us to upgrade spring-cloud to 2.1.4 and remove a circular
    dependency from our @Configuration beans

--- src/main/java/com/aerospike/restclient/config/AerospikeClientConfig.java
+++ src/main/java/com/aerospike/restclient/config/AerospikeClientConfig.java
@@ -23,11 +23,9 @@ import com.aerospike.client.policy.ClientPolicy;
 import com.aerospike.restclient.util.AerospikeClientPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.Nullable;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 
@@ -47,17 +45,11 @@ public class AerospikeClientConfig {
     @Value("${aerospike.restclient.requireAuthentication:false}")
     boolean requireAuthentication;
 
-    @Value("${aerospike.restclient.useBoolBin:false}")
-    boolean useBoolBin;
-
-    @Autowired
-    ClientPolicy policy;
-
     @Bean
     @Retryable(
             value = {AerospikeException.class}, backoff = @Backoff(delay = 2000, multiplier = 2)
     )
-    public AerospikeClient configAerospikeClient() {
+    public AerospikeClient configAerospikeClient(ClientPolicy policy) {
         if (requireAuthentication) {
             return null;
         }
@@ -77,15 +69,16 @@ public class AerospikeClientConfig {
         return client;
     }
 
+    @Value("${aerospike.restclient.useBoolBin:false}")
+    boolean useBoolBin;
+
     @Value("${aerospike.restclient.pool.size:16}")
     int poolSize;
 
-    @Autowired
-    @Nullable
-    AerospikeClient defaultClient;
-
     @Bean
-    public AerospikeClientPool configAerospikeClientPool() {
+    public AerospikeClientPool configAerospikeClientPool(ClientPolicy policy, AerospikeClient defaultClient) {
         return new AerospikeClientPool(poolSize, policy, port, hostList, hostname, defaultClient, useBoolBin);
     }
 }
+
+
